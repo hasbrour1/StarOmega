9c72bf28040437c239de69eaccdeb006b4bd9330
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-06-10 13:20:28 -0400

    Fixed errors in World class

--- core/src/com/hasbrouckproductions/rhasbrouck/games/MainShip.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/MainShip.java
@@ -4,7 +4,9 @@ package com.hasbrouckproductions.rhasbrouck.games;
  * Created by hasbrouckr on 6/10/2016.
  */
 public class MainShip extends DynamicGameObject {
+    public static final int SHIP_STATE_ALIVE = 2;
     public static final int SHIP_STATE_HIT = 2;
+    public static final int SHIP_DEAD = 3;
     public static final float SHIP_MOVE_VELOCITY = 20;
     public static final float SHIP_WIDTH = 0.8f;
     public static final float SHIP_HEIGHT = 0.8f;
@@ -17,6 +19,7 @@ public class MainShip extends DynamicGameObject {
     public MainShip (float x, float y) {
         super(x, y, SHIP_WIDTH, SHIP_HEIGHT);
         stateTime = 0;
+        state = SHIP_STATE_ALIVE;
         powerUp = 0;
     }
 
@@ -35,11 +38,15 @@ public class MainShip extends DynamicGameObject {
     public void gotHit () {
         velocity.set(0, 0);
         if(powerUp == 0) {
-            //game over
+            state = SHIP_DEAD;
         }
         else{
             powerUp--;
         }
         stateTime = 0;
+    }
+
+    public void gotPower(){
+        powerUp = 1;
     }
 }
--- core/src/com/hasbrouckproductions/rhasbrouck/games/World.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/World.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Vector2;
 
 public class World {
     public interface WorldListener {
-        public void shoot ();
+        public void shoot();
 
-        public void hit ();
+        public void hit();
     }
 
     public static final float WORLD_WIDTH = 10;
@@ -32,7 +33,7 @@ public class World {
     public int score;
     public int state;
 
-    public World (WorldListener listener) {
+    public World(WorldListener listener) {
         this.ship = new MainShip(5, 1);
         this.enemies = new ArrayList<Enemy>();
         this.powerUps = new ArrayList<PowerUps>();
@@ -44,42 +45,21 @@ public class World {
         this.state = WORLD_STATE_RUNNING;
     }
 
-    private void generateLevel () {
-        float y = Platform.PLATFORM_HEIGHT / 2;
-        float maxJumpHeight = Bob.BOB_JUMP_VELOCITY * Bob.BOB_JUMP_VELOCITY / (2 * -gravity.y);
-        while (y < WORLD_HEIGHT - WORLD_WIDTH / 2) {
-            int type = rand.nextFloat() > 0.8f ? Platform.PLATFORM_TYPE_MOVING : Platform.PLATFORM_TYPE_STATIC;
-            float x = rand.nextFloat() * (WORLD_WIDTH - Platform.PLATFORM_WIDTH) + Platform.PLATFORM_WIDTH / 2;
+    private void generateLevel() {
 
-            Platform platform = new Platform(type, x, y);
-            platforms.add(platform);
+        //For now create 5 enemies and 1 power up
+        enemies.add(new Enemy(100, 100));
+        enemies.add(new Enemy(200, 50));
+        enemies.add(new Enemy(300, 200));
+        enemies.add(new Enemy(400, 100));
+        enemies.add(new Enemy(500, 150));
 
-            if (rand.nextFloat() > 0.9f && type != Platform.PLATFORM_TYPE_MOVING) {
-                Spring spring = new Spring(platform.position.x, platform.position.y + Platform.PLATFORM_HEIGHT / 2
-                        + Spring.SPRING_HEIGHT / 2);
-                springs.add(spring);
-            }
+        powerUps.add(new PowerUps(200, 650));
 
-            if (y > WORLD_HEIGHT / 3 && rand.nextFloat() > 0.8f) {
-                Squirrel squirrel = new Squirrel(platform.position.x + rand.nextFloat(), platform.position.y
-                        + Squirrel.SQUIRREL_HEIGHT + rand.nextFloat() * 2);
-                squirrels.add(squirrel);
-            }
 
-            if (rand.nextFloat() > 0.6f) {
-                Coin coin = new Coin(platform.position.x + rand.nextFloat(), platform.position.y + Coin.COIN_HEIGHT
-                        + rand.nextFloat() * 3);
-                coins.add(coin);
-            }
-
-            y += (maxJumpHeight - 0.5f);
-            y -= rand.nextFloat() * (maxJumpHeight / 3);
-        }
-
-        castle = new Castle(WORLD_WIDTH / 2, y);
     }
 
-    public void update (float deltaTime, float accelX) {
+    public void update(float deltaTime, float accelX) {
         updateShip(deltaTime, accelX);
         updateEnemies(deltaTime);
         updatePowerUps(deltaTime);
@@ -87,78 +67,57 @@ public class World {
         checkGameOver();
     }
 
-    private void updateShip (float deltaTime, float accelX) {
-        if (ship.state != ship.SHIP_STATE_HIT) ship.velocity.x = -accelX / 10 * ship.SHIP_MOVE_VELOCITY;
+    private void updateShip(float deltaTime, float accelX) {
+        if (ship.state != ship.SHIP_STATE_HIT)
+            ship.velocity.x = -accelX / 10 * ship.SHIP_MOVE_VELOCITY;
         ship.update(deltaTime);
     }
 
-    private void updateEnemies (float deltaTime) {
-        int len = squirrels.size();
+    private void updateEnemies(float deltaTime) {
+        int len = enemies.size();
         for (int i = 0; i < len; i++) {
-            Squirrel squirrel = squirrels.get(i);
-            squirrel.update(deltaTime);
+            Enemy enemy = enemies.get(i);
+            enemy.update(deltaTime);
         }
     }
 
-    private void updatePowerUps (float deltaTime) {
-        int len = coins.size();
+    private void updatePowerUps(float deltaTime) {
+        int len = powerUps.size();
         for (int i = 0; i < len; i++) {
-            Coin coin = coins.get(i);
-            coin.update(deltaTime);
+            PowerUps power = powerUps.get(i);
+            power.update(deltaTime);
         }
     }
 
-    private void checkCollisions () {
-        checkSquirrelCollisions();
-        checkItemCollisions();
+    private void checkCollisions() {
+        checkPowerUpCollisions();
+        checkEnemyCollisions();
     }
 
-    private void checkSquirrelCollisions () {
-        int len = squirrels.size();
+    private void checkPowerUpCollisions() {
+        int len = powerUps.size();
         for (int i = 0; i < len; i++) {
-            Squirrel squirrel = squirrels.get(i);
-            if (squirrel.bounds.overlaps(bob.bounds)) {
-                bob.hitSquirrel();
+            PowerUps power = powerUps.get(i);
+            if (power.bounds.overlaps(ship.bounds)) {
+                ship.gotPower();
                 listener.hit();
             }
         }
     }
 
-    private void checkItemCollisions () {
-        int len = coins.size();
+    private void checkEnemyCollisions() {
+        int len = enemies.size();
         for (int i = 0; i < len; i++) {
-            Coin coin = coins.get(i);
-            if (bob.bounds.overlaps(coin.bounds)) {
-                coins.remove(coin);
-                len = coins.size();
-                listener.coin();
-                score += Coin.COIN_SCORE;
-            }
-
-        }
-
-        if (bob.velocity.y > 0) return;
-
-        len = springs.size();
-        for (int i = 0; i < len; i++) {
-            Spring spring = springs.get(i);
-            if (bob.position.y > spring.position.y) {
-                if (bob.bounds.overlaps(spring.bounds)) {
-                    bob.hitSpring();
-                    listener.highJump();
-                }
+            Enemy enemy = enemies.get(i);
+            if (ship.bounds.overlaps(enemy.bounds)) {
+                ship.gotHit();
             }
         }
     }
 
-    private void checkCastleCollisions () {
-        if (castle.bounds.overlaps(bob.bounds)) {
-            state = WORLD_STATE_NEXT_LEVEL;
-        }
-    }
-
-    private void checkGameOver () {
-        if (heightSoFar - 7.5f > bob.position.y) {
+    private void checkGameOver() {
+        if (ship.state == MainShip.SHIP_DEAD) {
             state = WORLD_STATE_GAME_OVER;
         }
     }
+}
