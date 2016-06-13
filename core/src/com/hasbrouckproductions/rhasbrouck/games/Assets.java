07b20c78cb1f92e0392678304baa9375e3372412
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-06-13 11:09:40 -0400

    Added Textures

--- core/src/com/hasbrouckproductions/rhasbrouck/games/Assets.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/Assets.java
@@ -1,18 +1,32 @@
 package com.hasbrouckproductions.rhasbrouck.games;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 
 /**
  * Created by hasbrouckr on 6/10/2016.
  */
 public class Assets {
 
+    public static Texture logo;
+    public static Texture backgroundRegion;
+
+    public static Sound clickSound;
+
     public static void load(){
+
+        backgroundRegion = new Texture(Gdx.files.internal("data/img/backgroundRegion.jpg"));
+        logo = new Texture(Gdx.files.internal("data/img/logo.png"));
 
     }
 
     public static Texture loadTexture(String loc){
         return new Texture(Gdx.files.internal(loc));
+    }
+
+    public static void playSound(Sound sound){
+        sound.play();
     }
 }
