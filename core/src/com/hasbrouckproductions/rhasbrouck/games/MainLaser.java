ae011e30728e2cb567d50e02977a3f6ada421c58
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-06-16 11:50:29 -0400

    Pushing MainLaser class

---
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/MainLaser.java
@@ -0,0 +1,41 @@
+package com.hasbrouckproductions.rhasbrouck.games;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.TimeUtils;
+
+/**
+ * Created by hasbrouckr on 6/16/2016.
+ */
+public class MainLaser extends DynamicGameObject{
+
+    public static final int LASER_NOT_HIT = 1;
+    public static final int LASER_HIT = 2;
+    public static final int LASER_WIDTH = 50;
+    public static final int LASER_HEIGHT = 10;
+
+    public static final int LASER_VELOCITY = 400;
+
+    public float xPos;
+    public float yPos;
+    public static float lastFireTime;
+    public int state;
+
+
+    public MainLaser(float x, float y){
+        super(x, y, LASER_WIDTH, LASER_HEIGHT);
+        xPos = x;
+        yPos = y;
+        state = LASER_NOT_HIT;
+        lastFireTime = TimeUtils.nanoTime();
+    }
+
+    public void update(){
+        xPos += LASER_VELOCITY * Gdx.graphics.getDeltaTime();
+        this.bounds = new Rectangle(xPos, yPos, LASER_WIDTH, LASER_HEIGHT);
+    }
+
+    public void hitEnemy(){
+        state = LASER_HIT;
+    }
+}
