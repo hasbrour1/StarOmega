3ea76bc650adde612d518b06e50ff20856509ade
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-06-21 11:44:20 -0400

    Updated Level 1 and 2

--- core/src/com/hasbrouckproductions/rhasbrouck/games/LevelTwo.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/LevelTwo.java
@@ -17,15 +17,18 @@ public class LevelTwo extends GenericLevel {
 
     @Override
     void generateEnemies() {
-        enemies.add(new Enemy(800, 200));
-        enemies.add(new Enemy(800 * 2 + 56, 50));
-        enemies.add(new Enemy(830 * 2, 320));
+        enemies.add(new Enemy(800, 100));
+        enemies.add(new Enemy(850, 75));
+        enemies.add(new Enemy(860, 130));
+        enemies.add(new Enemy(800 * 2, 50));
+        enemies.add(new Enemy(830 * 2, 200));
+        enemies.add(new Enemy(800 * 2 + 80, 150));
+        enemies.add(new Enemy(830 * 2 + 15, 300));
+        enemies.add(new Enemy(830 * 2 + 90, 100));
         enemies.add(new Enemy(800 * 3 + 100, 100));
         enemies.add(new Enemy(800 * 3, 300));
-        enemies.add(new Enemy(800 * 4 + 200, 100));
-        enemies.add(new Enemy(800 * 4, 300));
-        enemies.add(new Enemy(800 * 4, 100));
-        enemies.add(new Enemy(800 * 4, 50));
+        enemies.add(new Enemy(800 * 3 + 100, 100));
+        enemies.add(new Enemy(800 * 3 + 50, 175));
     }
 
     @Override
