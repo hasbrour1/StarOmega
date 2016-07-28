738251190cb9d16cb097edcdb9e2389907646710
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-07-28 09:17:39 -0400

    Made level 1 a little easier

--- core/src/com/hasbrouckproductions/rhasbrouck/games/LevelOne.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/LevelOne.java
@@ -14,10 +14,8 @@ public class LevelOne extends GenericLevel {
         enemies.add(new Enemy(810, 130));
         enemies.add(new Enemy(860, 220));
         enemies.add(new Enemy(800 * 2, 50));
-        enemies.add(new Enemy(830 * 2, 200));
         enemies.add(new Enemy(800 * 2 - 80, 150));
         enemies.add(new Enemy(800 * 3 + 75,300));
-        enemies.add(new Enemy(800 * 3 + 120, 90));
         enemies.add(new Enemy(800 * 3 + 50, 175));
     }
 
