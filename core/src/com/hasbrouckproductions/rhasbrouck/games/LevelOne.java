5a7bbf393d8a0c3dab59e4f1abe97236151f5693
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-10-10 10:59:36 -0400

    Tweaked level one to have less space between enemy's

--- core/src/com/hasbrouckproductions/rhasbrouck/games/LevelOne.java
+++ core/src/com/hasbrouckproductions/rhasbrouck/games/LevelOne.java
@@ -9,14 +9,16 @@ public class LevelOne extends GenericLevel {
     void generateEnemies() {
         enemies.add(new Enemy(810, 130));
         enemies.add(new Enemy(860, 220));
+        enemies.add(new Enemy(930, 30));
+        enemies.add(new Enemy(1020, 110));
         enemies.add(new Enemy(800 * 2, 50));
-        enemies.add(new Enemy(800 * 2 - 80, 200));
-        enemies.add(new Enemy(800 * 3 + 75,300));
-        enemies.add(new Enemy(800 * 3 + 50, 175));
+        enemies.add(new Enemy(800 * 2 - 200, 200));
+        enemies.add(new Enemy(800 * 2 + 75,300));
+        enemies.add(new Enemy(800 * 2 + 50, 175));
     }
 
     @Override
     void generateBoss() {
-        boss = new BossOne(800 * 3 + 300, 50);
+        boss = new BossOne(800 * 2 + 300, 50);
     }
 }
